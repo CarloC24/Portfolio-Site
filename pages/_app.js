12cd5deb74fce38ee065110537e660cbaa6b04c9
Carlo Clamucha <carlo.clamucha@gmail.com>
2019-02-20 11:45:31 -0600

    init on portfolio site

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+import App, { Container } from 'next/app';
+import Head from '../components/Head';
+import Router from 'next/router';
+import Page from '../components/Page';
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    //this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
