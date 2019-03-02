558ec97c164796ea84cf3a8b744f3d82058e91a3
Juan Carlo Clamucha <carloclamucha@Carlo-Macbook.local>
2019-03-02 13:56:48 -0600

    added homepage

---
+++ Components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is the homepage</p>
+      </div>
+    )
+  }
+}
+
+export default HomePage
