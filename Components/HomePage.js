713c9919c42c7f0ba60b8bc837727ee5b945b90d
Juan Carlo Clamucha <carloclamucha@Carlo-Macbook.local>
2019-03-03 21:20:44 -0600

    coolio

--- Components/HomePage.js
+++ Components/HomePage.js
@@ -4,6 +4,8 @@ export class HomePage extends Component {
   render() {
     return (
       <div>
+        <p>Hi my name is Juan Carlo Clamucha</p>
+        <p>Web Developer</p>
         <p>this is the homepage</p>
       </div>
     )
