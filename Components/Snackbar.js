cfeaefb08aa3bcfda186d4f234d29b278b48b237
CarloC24 <carlo.clamucha@gmail.com>
2020-04-27 23:18:25 -0700

    adding snackbar

---
+++ Components/Snackbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Styled from "styled-components";
+
+const SnackbarDiv = styled.div`
+
+`
+
+ class Snackbar extends Component {
+    render() {
+        return (
+            <SnackbarDiv className="d-flex">
+                <p>awda</p>
+            </SnackbarDiv>
+        )
+    }
+}
+
+
+export default Snackbar;
