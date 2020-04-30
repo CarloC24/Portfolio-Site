aa1538d36c32fb5c842fc8f66a51c6778548a060
CarloC24 <carlo.clamucha@gmail.com>
2020-04-29 23:40:51 -0700

    refactoring snackbar

--- Components/Snackbar.js
+++ Components/Snackbar.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Styled from "styled-components";
-import logosswitch from "../assets/logosswitch";
+import logosSwitch from "../assets/logosswitch";
 
-const SnackbarDiv = styled.div`
-
-`
-
- class Snackbar extends Component {
-    render() {
-        return (
-            <SnackbarDiv className="d-flex">
-                <p>Github</p>
-                <p>Resume</p>
-                <p>LinkedIn</p>
-                <p>Email</p>
-            </SnackbarDiv>
-        )
-    }
+const SnackbarDiv = Styled.div`
+position:absolute;
+img {
+  width:50px;
 }
+`;
 
+class Snackbar extends Component {
+  render() {
+    return (
+      <>
+        <SnackbarDiv className="d-flex flex-column">
+          {logosSwitch("email")}
+          {logosSwitch(
+            "linkedIn",
+            "https://www.linkedin.com/in/juan-carlo-clamucha-1489b216b/"
+          )}
+          {logosSwitch("github", "https://github.com/CarloC24")}
+          {logosSwitch(
+            "resume",
+            "https://resume.creddle.io/resume/fwoe7ya3wlk"
+          )}
+        </SnackbarDiv>
+      </>
+    );
+  }
+}
 
 export default Snackbar;
