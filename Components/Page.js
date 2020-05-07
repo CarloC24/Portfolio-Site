11158822d71ee60b8c87ede848eee43040fa896d
CarloC24 <carlo.clamucha@gmail.com>
2020-05-06 23:53:28 -0700

    added fixes for medium size

--- Components/Page.js
+++ Components/Page.js
@@ -5,6 +5,9 @@ import Snackbar from "./Snackbar";
 const PageContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 10fr;
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export default class extends React.Component {
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import Navbar from "../Components/Navbar";
 import AboutPage from "../Components/AboutPage";
 import Projects from "../Components/Projects";
+import HorizontalSnackbar from "../Components/HorizontalSnackbar";
 
 export class HomePage extends Component {
   render() {
     return (
-      <div>
+      <div className="w-100">
         <Projects />
+        <HorizontalSnackbar />
       </div>
     );
   }
