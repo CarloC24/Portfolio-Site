c39910f3a22d09d8c320b67c7e48b1d495f09f32
CarloC24 <carlo.clamucha@gmail.com>
2020-05-13 21:16:44 -0700

    adding a scroll down listener

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,29 @@ export class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      componentCount: 1,
+      yOffset: 0,
     };
   }
+  componentDidMount() {
+    // add incremeting scroll downs here........
+    window.onscroll = () => {
+      if (window.pageYOffset === this.state.yOffset) {
+        alert("user has scrolled to the bottom");
+      } else {
+        this.setState((prevState) => ({
+          ...prevState,
+          yOffset: window.pageYOffset,
+        }));
+      }
+      // if (
+      //   window.scrollTop() + window.height() >
+      //   window.document.height() - 100
+      // ) {
+      //   alert("scrolled at the bottom");
+      // }
+    };
+  }
+
   render() {
     return (
       <div className="w-100">
