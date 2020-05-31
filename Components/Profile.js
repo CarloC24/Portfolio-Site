41f8894adb73adfc91c7fd35e13195225df7ba3f
CarloC24 <carlo.clamucha@gmail.com>
2020-05-31 16:03:47 -0500

    adding my changes for now

--- Components/Profile.js
+++ Components/Profile.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Styled from "styled-components";
+
+const ProfileContainer = Styled.div`
+p,b {
+    font-size:30px;
+}
+`;
 
 export default class Profile extends Component {
-    render() {
-        return (
-            <div>
-                <p>This is the profile page</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <ProfileContainer>
+        <b>Hello I'm Carlo</b>
+        <p>I am a software engineer</p>
+      </ProfileContainer>
+    );
+  }
 }
